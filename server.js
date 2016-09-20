3b1ff82d057cb9e0b694a677eb6027e7e9226141
sdas8172 <sdas8172@gmail.com>
2016-09-20 09:31:05 -0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,15 +10,15 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article1', function (req,res){
-   res.sendFile(path.join(_dirname,'ui','article1.html')); 
+   res.sendFile(path.join(__dirname,'ui','article1.html')); 
 });
 
 app.get('/article2', function (req,res){
-   res.sendFile(path.join(_dirname,'ui','article2.html')); 
+   res.sendFile(path.join(__dirname,'ui','article2.html')); 
 });
 
 app.get('/article3', function (req,res){
-   res.sendFile(path.join(_dirname,'ui','article3.html')); 
+   res.sendFile(path.join(__dirname,'ui','article3.html')); 
 });
 
 app.get('/ui/style.css', function (req, res) {
