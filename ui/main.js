a1818bdaa38cb46d9a5c9d4c86fd363e5fc31455
sdas8172 <sdas8172@gmail.com>
2016-09-21 00:10:45 -0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,12 @@
 console.log('Loaded!');
+
+
+//move image
+var img=document.getElementById('web');
+function moveRight() {
+    merginLeft = merginLeft + 1;
+    img.style.merginLeft = merginLeft + 'px';
+}
+img.onclick = function() {
+    var interval = setInterval(moveRight, 50);
+};
