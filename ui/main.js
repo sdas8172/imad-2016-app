e1db65ce9662e96b6c0e49dd5021126c3c2244dd
sdas8172 <sdas8172@gmail.com>
2016-09-30 00:15:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,22 @@
 console.log('Loaded!');
 
+//counter code
+var button = doccument.getElementById('counter');
+var counter=0;
+button.onclick=function(){
+    //make a request to the counter end point
+    
+    //capture the reponse and store in a variable
+    
+    //render the variable in the correct span
+    counter=counter+1;
+    var span= doccument.getElementById('count');
+    span.innerHTML=counter.toString();
+};
+
+
+
+
 
 //move image
 var img=document.getElementById('web');
@@ -12,16 +29,3 @@ img.onclick = function() {
     var interval = setInterval(moveRight, 50);
 };
 
-//counter code
-var button = doccument.getElementById('counter');
-var counter=0;
-button.onclick=function(){
-    //make a request to the counter end point
-    
-    //capture the reponse and store in a variable
-    
-    //render the variable in the correct span
-    counter=counter+1;
-    var span= doccument.getElementById('count');
-    span.innerHtml=counter.toString();
-};
