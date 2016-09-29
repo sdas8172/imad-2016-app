3f804af8f9f1589da70f6ad6e13f329f747da00d
sdas8172 <sdas8172@gmail.com>
2016-09-30 01:27:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -26,6 +26,24 @@ button.onclick=function(){
     
 };
 
+//submit name
+var nameInput = document.getelementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit_btn');
+submit.onclick = function(){
+    // make a request to the server and send the name
+    
+    // capture a list of name and render it as a list
+    var names =['name1','name2','name3'];
+    var list= '';
+    for(var i=0;i<names.length;i++)
+    {
+        list += '<li>'+names[i]+'</li>';
+    }
+    var ul= document.getElementById("namelist");
+    ul.innerHTML=list;
+};
+
 
 
 
