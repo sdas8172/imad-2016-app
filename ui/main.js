35dd4c6401008a6784188e31a10b25826342c69d
sdas8172 <sdas8172@gmail.com>
2016-09-30 09:56:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -32,16 +32,33 @@ var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function(){
     // make a request to the server and send the name
+     var request= new XMLHttpRequest();
     
-    // capture a list of name and render it as a list
-    var names =['name1','name2','name3'];
-    var list= '';
-    for(var i=0;i<names.length;i++)
-    {
-        list += '<li>'+names[i]+'</li>';
-    }
-    var ul= document.getElementById('namelist');
-    ul.innerHTML=list;
+    //capture the reponse and store in a variable
+    request.onreadystatechange = function(){
+      if(request.readyState === XMLHttpRequest.DONE){
+          //take a action
+          if(request.status ===200){
+              // capture a list of name and render it as a list
+                var names = request.responseText;
+                names= JSON.parse(names);
+                var list= '';
+                for(var i=0;i<names.length;i++)
+                {
+                    list += '<li>'+names[i]+'</li>';
+                }
+                var ul= document.getElementById('namelist');
+                ul.innerHTML=list;
+          }
+      }  
+      //not done yet
+    };
+    
+    //make a request
+    request.open('GET','http://sdas8172.imad.hasura-app.io/submit-name?name=' + name,true);
+    request.send(null);
+    
+    
 };
 
 
