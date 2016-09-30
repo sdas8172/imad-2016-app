5da889a19eeb75d22feb31cde4175dfa7137006b
sdas8172 <sdas8172@gmail.com>
2016-09-30 09:42:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -80,6 +80,15 @@ return htmlTemplate;
 
 }
 
+var names=[];
+app.get('/submit-name', function(req,res){// /submit-name?name=xxxx
+   //get the name from the request
+   var name=req.query.name;
+   
+   names.push(name);
+   //JSON: java script object notation
+   res.send(JSON.stringify(names));
+});
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
@@ -118,15 +127,7 @@ app.get('/ui/wall.jpg', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'wall.jpg'));
 });
 
-var names=[]
-app.get('/submit-name', function(req,res){// /submit-name?name=xxxx
-   //get the name from the request
-   var name=req.query.name;
-   
-   names.push(name);
-   //JSON: java script object notation
-   res.send(JSON.stringify(names));
-});
+
 
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
