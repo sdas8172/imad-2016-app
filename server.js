de87a3fb9b210b633a4e3dba28ea85b5861b3672
sdas8172 <sdas8172@gmail.com>
2016-11-05 10:30:38 -0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -212,7 +212,7 @@ app.get('/test-db', function(req, res){
           res.status(500).send(err.toString());
       } 
       else {
-          res.send(JSON.stringify(result));
+          res.send(JSON.stringify(result.rows));
       }
    });
     
