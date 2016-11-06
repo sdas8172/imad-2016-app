12e08c2104fc1baf3bce25bf4d1a7ce30a9d70b2
sdas8172 <sdas8172@gmail.com>
2016-11-06 01:33:13 -0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -51,6 +51,7 @@ var articles = {
 },
     "training": {
         title: "FIND your training | dailyfit",
+        date: "nov 6 2016",
         heading: "FIND training with YOUR choice",
         content: `<div>
          location<input type="text">
